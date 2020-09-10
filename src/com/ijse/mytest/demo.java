772d523da76000637cd8c75d5df96086b20af1b1
Isuru <ipjayawick@gmail.com>
2020-09-10 10:56:16 +0530

    4th commit

--- src/com/ijse/mytest/demo.java
+++ src/com/ijse/mytest/demo.java
@@ -5,6 +5,6 @@ import java.sql.SQLOutput;
 public class demo {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hi");
+        System.out.println("this is new branch?");
     }
 }
