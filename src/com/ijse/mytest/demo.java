93e1063cf7ccedd4ddf2fe563e528a86167eacce
Isuru <ipjayawick@gmail.com>
2020-09-10 10:50:50 +0530

    4th commit

--- src/com/ijse/mytest/demo.java
+++ src/com/ijse/mytest/demo.java
@@ -1,7 +1,10 @@
 package com.ijse.mytest;
 
+import java.sql.SQLOutput;
+
 public class demo {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hi");
     }
 }
